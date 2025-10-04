665ec90983587132668f69c83639446b3245586c
Edmund Miller <git@edmundmiller.dev>
2025-10-04 17:52:05 -0500

    chore: clean up aider chat history and build config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./pages/**/*.{html,js,svelte,md}', './.evidence/**/*.{html,js,svelte,md}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
